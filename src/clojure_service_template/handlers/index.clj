2b4e7091b0ec3c9cc24293c9fb0eb4e1d2e0dffd
Andrey Fadeev <andfadeev@gmail.com>
2025-01-20 22:02:59 +0000

    add simple dark mode toggle with alpine.js + tailwind (#3)

--- src/clojure_service_template/handlers/index.clj
+++ src/clojure_service_template/handlers/index.clj
@@ -1,29 +1,62 @@
 (ns clojure-service-template.handlers.index
   (:require [clojure-service-template.utils :as utils]
-            [hiccup.page :as hiccup-page]))
+            [hiccup.page :as hiccup-page]
+            [clojure-service-template.svg :as svg]))
+
+(defn service-version
+  []
+  (or (System/getenv "SERVICE_VERSION")
+      (random-uuid)))
+
+(defn with-service-version
+  [path]
+  (str path "?v=" (service-version)))
 
 (defn layout
   [body]
   [:head
    [:title "Clojure Service Template"]
    (hiccup-page/include-css
-    "/assets/css/output.css")
-   (hiccup-page/include-js
-    "https://unpkg.com/htmx.org@1.9.4")
+    (with-service-version "/assets/css/output.css"))
+   [:script {:src (with-service-version "/assets/js/theme.js")}]
+   [:script {:src "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"}]
+   [:script {:src "https://unpkg.com/htmx.org@2.0.4"
+             :integrity "sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+"
+             :crossorigin "anonymous"}]
    body])
+
+(defn theme-toggle
+  []
+  [:div
+   [:button {"@click" "$store.theme.lightTheme()"
+             :class "text-white rounded p-1 hover:text-yellow-500"
+             :x-show "$store.theme.dark"}
+    (svg/sun)]
+
+   [:button {"@click" "$store.theme.darkTheme()"
+             :class "text-gray-400 rounded p-1 hover:text-sky-900"
+             :x-show "!$store.theme.dark"}
+    (svg/moon)]])
+
+(defn nav
+  []
+  [:nav {:class "flex justify-between container py-5 items-center"}
+   [:a {:class "text-xl font-bold"} "Clojure Service Template"]
+   (theme-toggle)])
 
 (defn index
   [request]
   (let [user (utils/request->user request)
-        body [:body.bg-stone-950.text-white
-              [:div.container.mx-auto.px-5
+        body [:body {:class "bg-white dark:bg-black dark:text-white"}
+              (nav)
+              [:div.container
                (if user
                  [:div
                   [:h1.text-lg "Welcome"]
                   [:div (str user)]
                   [:div [:a {:href "/logout"} "Logout"]]]
                  [:div
-                  [:h1.text-xl.font-bold.text-green-500 "Login or Signup"]
+                  [:h1 {:class "text-xl font-bold text-green-500 dark:text-red-500"} "Login or Signup"]
                   [:div [:a {:href "/login"} "Login"]]
                   [:div [:a {:href "/signup"} "Signup"]]])]]]
     (->> body
