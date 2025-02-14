48bdfa760b8c019eb409bf45198569dbda4c276f
Andrey Fadeev <andfadeev@gmail.com>
2025-02-14 16:19:58 +0000

    upgrade deps

--- project.clj
+++ project.clj
@@ -9,33 +9,33 @@
                  [ring/ring-jetty-adapter "1.13.0"]
                  [com.stuartsierra/component "1.1.0"]
                  [aero "1.1.6"]
-                 [metosin/malli "0.16.4"]
-                 [hiccup/hiccup "2.0.0-RC3"]
-                 [com.kinde/kinde-core "2.0.0"]
+                 [metosin/malli "0.17.0"]
+                 [hiccup/hiccup "2.0.0-RC4"]
+                 [com.kinde/kinde-core "2.0.1"]
 
                  [camel-snake-kebab "0.4.3"]
-                 [org.babashka/http-client "0.3.11"]
-                 [com.cnuernber/charred "1.034"]
+                 [org.babashka/http-client "0.4.22"]
+                 [com.cnuernber/charred "1.036"]
 
-                 [com.github.seancorfield/next.jdbc "1.3.955"]
+                 [com.github.seancorfield/next.jdbc "1.3.994"]
                  [com.layerware/hugsql "0.5.3"]
-                 [com.github.seancorfield/honeysql "2.6.1203"]
-                 [org.postgresql/postgresql "42.7.4"]
-                 [org.flywaydb/flyway-core "10.21.0"]
-                 [org.flywaydb/flyway-database-postgresql "10.21.0"]
+                 [com.github.seancorfield/honeysql "2.6.1270"]
+                 [org.postgresql/postgresql "42.7.5"]
+                 [org.flywaydb/flyway-core "11.3.2"]
+                 [org.flywaydb/flyway-database-postgresql "11.3.2"]
 
-                 [com.zaxxer/HikariCP "6.0.0"]
+                 [com.zaxxer/HikariCP "6.2.1"]
 
                  [org.clojure/tools.logging "1.3.0"]
                  [org.slf4j/slf4j-api "2.1.0-alpha1"]
                  [org.slf4j/slf4j-simple "2.1.0-alpha1"]]
   :main ^:skip-aot clojure-service-template.core
   :target-path "target/%s"
-  :plugins [[com.github.liquidz/antq "2.10.1241"]]
+  :plugins [[com.github.liquidz/antq "2.11.1264"]]
   :antq {}
   :repl-options {:init-ns dev}
   :profiles {:dev {:dependencies [[com.stuartsierra/component.repl "1.0.0"]
-                                  [nrepl/nrepl "1.3.0"]
+                                  [nrepl/nrepl "1.3.1"]
                                   [org.testcontainers/testcontainers "1.20.4"]
                                   [org.testcontainers/postgresql "1.20.4"]]
                    :source-paths ["dev"]}
